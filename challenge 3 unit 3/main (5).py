1a02972dd1e65609ceec6e4625f486019996bc9d
JAYCEEAPC <144356006+JAYCEEAPC@users.noreply.github.com>
2023-10-08 16:52:49 +0530

    Add files via upload

---
+++ challenge 3 unit 3/main (5).py
@@ -0,0 +1,13 @@
+def linearSearchProduct(productList,targetProduct):
+  indices=[]
+  for index,product in enumerate(productList):
+     if product==targetProduct:
+       indices.append(index)
+  return indices
+products=["shoes","boot","loafers","shoes","sandal","shoes"]
+target="shoes"
+target2="apple"
+result1=linearSearchProduct(products,target)
+result2=linearSearchProduct(products,target2)
+print(result1)
+print(result2)
